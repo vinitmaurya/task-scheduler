ed443eb8f397ad3f47ab22789a1eddf3d81e6737
Vinit Maurya <vinitmaurya8898@gmail.com>
2024-06-07 18:02:36 +0530

    all initial files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListingComponent } from './task-listing/task-listing.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskComponent } from './task/task.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskListingComponent,
+    CreateTaskComponent,
+    TaskComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-listing/task-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-listing',
+  templateUrl: './task-listing.component.html',
+  styleUrl: './task-listing.component.css'
+})
+export class TaskListingComponent {
+
+}
